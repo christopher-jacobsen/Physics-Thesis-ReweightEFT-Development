43cb1732dcfee8865b987f750ac8513ecad010e1
Christopher Jacobsen <cja@phaseone.com>
2015-10-03 17:00:31 +0200

    ReweightEFT: main.cpp 10fb^-1 observables.

--- ReweightEFT/main.cpp
+++ ReweightEFT/main.cpp
@@ -18,36 +18,37 @@ using namespace ReweightEFT;
 
 static const ModelCompare::ObservableVector Observables1 =
 {
-//    { "PTZ", "P_{T}(Z)",  300,     0, 1500, "P_{T}(Z) [GeV/c]", "Events per 5 GeV/c",       OBSFILL{ RootUtil::FillHistPT(  h, w, s, 24);     } },
-//    { "MWZ", "M(WZ)",     250,     0, 3750, "M(WZ) [GeV/c^2]",  "Events per 15 GeV/c^2",    OBSFILL{ RootUtil::FillHistMass(h, w, s, 24, 23); } },
-//    { "PTZ", "P_{T}(Z)",  100,     0,  500, "P_{T}(Z) [GeV/c]", "Events per 5 GeV/c",       OBSFILL{ RootUtil::FillHistPT(  h, w, s, 24);     } },
-//    { "MWZ", "M(WZ)",     100,     0, 1500, "M(WZ) [GeV/c^2]",  "Events per 15 GeV/c^2",    OBSFILL{ RootUtil::FillHistMass(h, w, s, 24, 23); } },
+    // phase-space observables
 
-//    { "PTZ", "P_{T}(Z)",  150,     0,  750, "P_{T}(Z) [GeV/c]", "Events per 5 GeV/c",       OBSFILL{ RootUtil::FillHistPT(  h, w, s, 24);     } },
-//    { "MWZ", "M(WZ)",     140,     0, 2800, "M(WZ) [GeV/c^2]",  "Events per 20 GeV/c^2",    OBSFILL{ RootUtil::FillHistMass(h, w, s, 24, 23); } },
-//    { "RAZ", "Y(Z)",      100,    -5,    5, "Y(Z)",             "Events per bin",           OBSFILL{ RootUtil::FillHistRap( h, w, s, 24);     } },
-//    { "ETZ", "#eta(Z)",   100,   -10,   10, "#eta(Z)",          "Events per bin",           OBSFILL{ RootUtil::FillHistEta( h, w, s, 24);     } },
-//    { "PHZ", "#phi(Z)",   100, -M_PI, M_PI, "#phi(Z)",          "Events per bin",           OBSFILL{ RootUtil::FillHistPhi( h, w, s, 24);     } },
+    { "PTZ", "P_{T}(Z)",  150,     0,  750, "P_{T}(Z) [GeV/c]", "Events per 5 GeV/c",       OBSFILL{ RootUtil::FillHistPT(  h, w, s, 24);     } },
+    { "MWZ", "M(WZ)",     140,     0, 2800, "M(WZ) [GeV/c^2]",  "Events per 20 GeV/c^2",    OBSFILL{ RootUtil::FillHistMass(h, w, s, 24, 23); } },
+    { "RAZ", "Y(Z)",      100,    -5,    5, "Y(Z)",             "Events per bin",           OBSFILL{ RootUtil::FillHistRap( h, w, s, 24);     } },
+    { "ETZ", "#eta(Z)",   100,   -10,   10, "#eta(Z)",          "Events per bin",           OBSFILL{ RootUtil::FillHistEta( h, w, s, 24);     } },
+    { "PHZ", "#phi(Z)",   100, -M_PI, M_PI, "#phi(Z)",          "Events per bin",           OBSFILL{ RootUtil::FillHistPhi( h, w, s, 24);     } },
 
-    { "cWWW_O1",    "O_{1}(cWWW)",  1000, -6E4,  7E3,    "O_{1}(cWWW)",   "Events per bin",  OBSFILL{ FillHistOpt(h, w, s, "F_0_1_ocWWW"); } },
-    { "cWWW_O2",    "O_{2}(cWWW)",  1000,  0,   2E12,    "O_{2}(cWWW)",   "Events per bin",  OBSFILL{ FillHistOpt(h, w, s, "F_1_1_ocWWW"); } },
+    // optimal observables
 
-    { "cW_O1",      "O_{1}(cW)",    1000, -2E6,  2E4,    "O_{1}(cW)",     "Events per bin",  OBSFILL{ FillHistOpt(h, w, s, "F_0_2_ocW");   } },
-    { "cW_O2",      "O_{2}(cW)",    1000,  0,   6E11,    "O_{2}(cW)",     "Events per bin",  OBSFILL{ FillHistOpt(h, w, s, "F_2_2_ocW");   } },
+    { "cWWW_O1",    "O_{1}(cWWW)",  1000, -6E4,  7E3,    "O_{1}(cWWW)",   "Events per bin",  OBSFILL{ ReweightEFT::FillHistOpt(h, w, s, "F_0_1_ocWWW"); } },
+    { "cWWW_O2",    "O_{2}(cWWW)",  1000,  0,   2E12,    "O_{2}(cWWW)",   "Events per bin",  OBSFILL{ ReweightEFT::FillHistOpt(h, w, s, "F_1_1_ocWWW"); } },
 
-    { "cB_O1",      "O_{1}(cB)",    1000, -8E2, 5E3,     "O_{1}(cB)",     "Events per bin",  OBSFILL{ FillHistOpt(h, w, s, "F_0_3_ocB");   } },
-    { "cB_O2",      "O_{2}(cB)",    1000,  0,   2E8,     "O_{2}(cB)",     "Events per bin",  OBSFILL{ FillHistOpt(h, w, s, "F_3_3_ocB");   } },
+    { "cW_O1",      "O_{1}(cW)",    1000, -2E6,  2E4,    "O_{1}(cW)",     "Events per bin",  OBSFILL{ ReweightEFT::FillHistOpt(h, w, s, "F_0_2_ocW");   } },
+    { "cW_O2",      "O_{2}(cW)",    1000,  0,   6E11,    "O_{2}(cW)",     "Events per bin",  OBSFILL{ ReweightEFT::FillHistOpt(h, w, s, "F_2_2_ocW");   } },
 
-    { "s",          "#sqrt{s}",     100, 0, 3000,   "#sqrt{s} [GeV]", "Events per bin",     OBSFILL{ FillHistSqrtS(h, w, s); } },
+    { "cB_O1",      "O_{1}(cB)",    1000, -8E2, 5E3,     "O_{1}(cB)",     "Events per bin",  OBSFILL{ ReweightEFT::FillHistOpt(h, w, s, "F_0_3_ocB");   } },
+    { "cB_O2",      "O_{2}(cB)",    1000,  0,   2E8,     "O_{2}(cB)",     "Events per bin",  OBSFILL{ ReweightEFT::FillHistOpt(h, w, s, "F_3_3_ocB");   } },
 
-    { "cWWW_O1vS",  "O_{1}(cWWW)",  100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{1}(cWWW)",   OBSFILL{ FillHistOpt_vs_sqrtS(h, w, s, "F_0_1_ocWWW"); },   ModelCompare::DefaultTProfileFactory },
-    { "cWWW_O2vS",  "O_{2}(cWWW)",  100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{2}(cWWW)",   OBSFILL{ FillHistOpt_vs_sqrtS(h, w, s, "F_1_1_ocWWW"); },   ModelCompare::DefaultTProfileFactory },
+    // mean optimal observables
 
-    { "cW_O1vS",    "O_{1}(cW)",    100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{1}(cW)",     OBSFILL{ FillHistOpt_vs_sqrtS(h, w, s, "F_0_2_ocW"); },     ModelCompare::DefaultTProfileFactory },
-    { "cW_O2vS",    "O_{2}(cW)",    100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{2}(cW)",     OBSFILL{ FillHistOpt_vs_sqrtS(h, w, s, "F_2_2_ocW"); },     ModelCompare::DefaultTProfileFactory },
+    { "s",          "#sqrt{s}",     100, 0, 3000,   "#sqrt{s} [GeV]", "Events per bin",     OBSFILL{ ReweightEFT::FillHistSqrtS(h, w, s); } },
 
-    { "cB_O1vS",    "O_{1}(cB)",    100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{1}(cB)",     OBSFILL{ FillHistOpt_vs_sqrtS(h, w, s, "F_0_3_ocB"); },     ModelCompare::DefaultTProfileFactory },
-    { "cB_O2vS",    "O_{2}(cB)",    100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{2}(cB)",     OBSFILL{ FillHistOpt_vs_sqrtS(h, w, s, "F_3_3_ocB"); },     ModelCompare::DefaultTProfileFactory },
+    { "cWWW_O1vS",  "O_{1}(cWWW)",  100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{1}(cWWW)",   OBSFILL{ ReweightEFT::FillHistOpt_vs_sqrtS(h, w, s, "F_0_1_ocWWW"); },   ModelCompare::DefaultTProfileFactory },
+    { "cWWW_O2vS",  "O_{2}(cWWW)",  100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{2}(cWWW)",   OBSFILL{ ReweightEFT::FillHistOpt_vs_sqrtS(h, w, s, "F_1_1_ocWWW"); },   ModelCompare::DefaultTProfileFactory },
+
+    { "cW_O1vS",    "O_{1}(cW)",    100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{1}(cW)",     OBSFILL{ ReweightEFT::FillHistOpt_vs_sqrtS(h, w, s, "F_0_2_ocW"); },     ModelCompare::DefaultTProfileFactory },
+    { "cW_O2vS",    "O_{2}(cW)",    100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{2}(cW)",     OBSFILL{ ReweightEFT::FillHistOpt_vs_sqrtS(h, w, s, "F_2_2_ocW"); },     ModelCompare::DefaultTProfileFactory },
+
+    { "cB_O1vS",    "O_{1}(cB)",    100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{1}(cB)",     OBSFILL{ ReweightEFT::FillHistOpt_vs_sqrtS(h, w, s, "F_0_3_ocB"); },     ModelCompare::DefaultTProfileFactory },
+    { "cB_O2vS",    "O_{2}(cB)",    100, 0, 3000,   "#sqrt{s} [GeV]", "Mean O_{2}(cB)",     OBSFILL{ ReweightEFT::FillHistOpt_vs_sqrtS(h, w, s, "F_3_3_ocB"); },     ModelCompare::DefaultTProfileFactory },
 
 //    { "cWWW_O2dS2vS", "O_{2}(cWWW)",  100, 0, 3000,  "#sqrt{s}",   "Mean O_{2}(cWWW)/s^{2}",  OBSFILL{ FillHistO2divS2_vs_sqrtS(h, w, s, "F_1_1_ocWWW"); }, ModelCompare::DefaultTProfileFactory },
 };
@@ -101,8 +102,8 @@ int main(void)
 {
   //ReweightEFT::ReweightEFT( "reweight/EFT_all_to_self_1E4.root", Observables1, CoefNames_EFT_all, Models_1E4[1], Params_EFT_all, Models_1E4[1], Params_EFT_all );
 
-    ReweightEFT::ReweightEFT( "reweight/EFT_all_to_SM_1E4.root", Observables1, CoefNames_EFT_all, Models_1E4[0], Params_EFT_SM, Models_1E4[1], Params_EFT_all );
-  //ReweightEFT::ReweightEFT( "reweight/EFT_all_to_SM_1E6.root", Observables1, CoefNames_EFT_all, Models_1E6[0], Params_EFT_SM, Models_1E6[1], Params_EFT_all );
+  //ReweightEFT::ReweightEFT( "reweight/EFT_all_to_SM_1E4.root", Observables1, CoefNames_EFT_all, Models_1E4[0], Params_EFT_SM, Models_1E4[1], Params_EFT_all );
+    ReweightEFT::ReweightEFT( "reweight/EFT_all_to_SM_1E6.root", Observables1, CoefNames_EFT_all, Models_1E6[0], Params_EFT_SM, Models_1E6[1], Params_EFT_all );
 
     LogMsgInfo( "\nDone." );
     return 0;
